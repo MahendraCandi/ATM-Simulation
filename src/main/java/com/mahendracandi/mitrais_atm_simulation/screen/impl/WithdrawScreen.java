ab6ad5310b2789a1e6bbddfe520223a43cf7867a
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-21 09:36:24 +0700

    add readInput

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/WithdrawScreen.java
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/WithdrawScreen.java
@@ -1,11 +1,23 @@
 package com.mahendracandi.mitrais_atm_simulation.screen.impl;
 
+import com.mahendracandi.mitrais_atm_simulation.model.Customer;
 import com.mahendracandi.mitrais_atm_simulation.screen.Screen;
 import com.mahendracandi.mitrais_atm_simulation.util.MessageUtil;
+import com.mahendracandi.mitrais_atm_simulation.util.ValidatorUtil;
+
+import java.math.BigDecimal;
 
 public class WithdrawScreen extends Screen {
 
+    private BigDecimal amount;
+    private Customer customer;
 
+    public WithdrawScreen() {
+    }
+
+    public WithdrawScreen(Customer customer) {
+        this.customer = customer;
+    }
 
     @Override
     public void showScreen() {
@@ -18,7 +30,41 @@ public class WithdrawScreen extends Screen {
         this.setDefaultInput("5");
     }
 
+    @Override
     public void readInput() {
+        String input = doInput();
+        BigDecimal amount = null;
+        switch (input) {
+            case "1":
+                amount = new BigDecimal("10");
+                break;
+            case "2":
+                amount = new BigDecimal("50");
+                break;
+            case "3":
+                amount = new BigDecimal("100");
+                break;
+            case "4":
+                OtherWithdrawScreen otherWithdrawScreen = new OtherWithdrawScreen();
+                otherWithdrawScreen.showScreen();
+                String otherAmount = otherWithdrawScreen.doInput();
+                ValidatorUtil validatorUtil = new ValidatorUtil();
+                boolean isWithdrawAmountValid = validatorUtil.isWithdrawAmountValid(otherAmount, customer);
+                if (isWithdrawAmountValid) {
+                    amount = new BigDecimal(otherAmount);
+                }
+                break;
+            case "5":
+                this.existScreen = true;
+                break;
+            default:
+                showScreen();
+                readInput();
+        }
+        this.amount = amount;
+    }
 
+    public BigDecimal getAmount() {
+        return this.amount;
     }
 }
