3554592e1705de55e168214138080bdccf370a76
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-21 13:09:07 +0700

    move TransactionScreen from app

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/TransactionScreen.java
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/TransactionScreen.java
@@ -1,10 +1,26 @@
 package com.mahendracandi.mitrais_atm_simulation.screen.impl;
 
+import com.mahendracandi.mitrais_atm_simulation.appEnum.TransactionType;
+import com.mahendracandi.mitrais_atm_simulation.model.Customer;
+import com.mahendracandi.mitrais_atm_simulation.model.Transaction;
 import com.mahendracandi.mitrais_atm_simulation.screen.Screen;
+import com.mahendracandi.mitrais_atm_simulation.util.ScreenUtil;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import static com.mahendracandi.mitrais_atm_simulation.util.MessageUtil.printMessage;
 
 public class TransactionScreen extends Screen {
+
+    private Customer customer;
+
+    public TransactionScreen() {
+    }
+
+    public TransactionScreen(Customer customer) {
+        this.customer = customer;
+    }
 
     @Override
     public void showScreen() {
@@ -16,7 +32,27 @@ public class TransactionScreen extends Screen {
     }
 
     @Override
-    protected void readInput() {
+    public void readInput() {
+        boolean exitLoop = false;
+        do {
+            showScreen();
+            String option = doInput();
+            switch (option) {
+                case "1":
+                    WithdrawScreen withdrawScreen = new WithdrawScreen(customer);
+                    withdrawScreen.readInput();
+                    if (withdrawScreen.isExistScreen()) continue;
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+                    exitLoop = true;
+                    this.existScreen = true;
+                default:
+                    readInput();
+            }
+        } while (!exitLoop);
 
     }
 
