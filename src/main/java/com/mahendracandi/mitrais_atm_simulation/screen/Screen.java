a02df3de310fa671b19bc8282cc305a3e52378ca
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-21 09:12:56 +0700

    add readInput(), input, exitScreen

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/Screen.java
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/Screen.java
@@ -3,10 +3,14 @@ package com.mahendracandi.mitrais_atm_simulation.screen;
 import java.util.Scanner;
 
 public abstract class Screen {
+    protected String input;
+    protected boolean existScreen = false;
     protected String defaultInput = "";
     protected Scanner scanner = new Scanner(System.in);
 
     public abstract void showScreen();
+
+    protected abstract void readInput();
 
     public String doInput() {
         String scanInput = this.scanner.nextLine();
@@ -22,6 +26,11 @@ public abstract class Screen {
         return this.defaultInput;
     }
 
-    protected abstract void readInput();
+    public String getInput() {
+        return input;
+    }
 
+    public boolean isExistScreen() {
+        return existScreen;
+    }
 }
