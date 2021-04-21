8745d48a0b51ca67af8a428f0a49a9bf313fba7b
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-21 14:02:41 +0700

    add insufficient balance validation

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/WithdrawScreen.java
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/WithdrawScreen.java
@@ -6,6 +6,7 @@ import com.mahendracandi.mitrais_atm_simulation.model.ScreenHelper;
 import com.mahendracandi.mitrais_atm_simulation.model.Transaction;
 import com.mahendracandi.mitrais_atm_simulation.screen.Screen;
 import com.mahendracandi.mitrais_atm_simulation.util.MessageUtil;
+import com.mahendracandi.mitrais_atm_simulation.util.ValidatorUtil;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 public class WithdrawScreen extends Screen {
 
     private Customer customer;
+    private final ValidatorUtil validatorUtil = new ValidatorUtil();
 
     public WithdrawScreen() {
     }
@@ -74,18 +76,23 @@ public class WithdrawScreen extends Screen {
         switch (input) {
             case "1":
                 amount = new BigDecimal("10");
+                if (!validateAmount(amount)) screenHelper.setInputNotValid(true);
                 break;
             case "2":
                 amount = new BigDecimal("50");
+                if (!validateAmount(amount)) screenHelper.setInputNotValid(true);
                 break;
             case "3":
                 amount = new BigDecimal("100");
+                if (!validateAmount(amount)) screenHelper.setInputNotValid(true);
                 break;
             case "4":
                 OtherWithdrawScreen otherWithdrawScreen = new OtherWithdrawScreen(customer);
                 otherWithdrawScreen.showScreen();
                 otherWithdrawScreen.readInput();
-                if(!otherWithdrawScreen.isExistScreen()) {
+                if(otherWithdrawScreen.isExistScreen()) {
+                    screenHelper.setInputNotValid(true);
+                } else {
                     amount = otherWithdrawScreen.getAmount();
                 }
                 break;
@@ -97,5 +104,9 @@ public class WithdrawScreen extends Screen {
         }
         screenHelper.setAmount(amount);
         return screenHelper;
+    }
+
+    private boolean validateAmount(BigDecimal amount) {
+        return validatorUtil.isCustomerHasSufficientBalance(customer, amount.intValue());
     }
 }
