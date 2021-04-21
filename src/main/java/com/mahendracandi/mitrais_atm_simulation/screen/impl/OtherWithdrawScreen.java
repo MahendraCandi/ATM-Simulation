b5e61bf9fbb1bbc2cb66c1ba950e27bebe7cb5e9
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-21 09:52:54 +0700

    add readInput to OtherWithdrawScreen

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/OtherWithdrawScreen.java
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/OtherWithdrawScreen.java
@@ -1,9 +1,24 @@
 package com.mahendracandi.mitrais_atm_simulation.screen.impl;
 
+import com.mahendracandi.mitrais_atm_simulation.model.Customer;
 import com.mahendracandi.mitrais_atm_simulation.screen.Screen;
 import com.mahendracandi.mitrais_atm_simulation.util.MessageUtil;
+import com.mahendracandi.mitrais_atm_simulation.util.ValidatorUtil;
+
+import java.math.BigDecimal;
 
 public class OtherWithdrawScreen extends Screen {
+
+    private BigDecimal amount;
+    private Customer customer;
+
+    public OtherWithdrawScreen() {
+    }
+
+    public OtherWithdrawScreen(Customer customer) {
+        this.customer = customer;
+    }
+
     @Override
     public void showScreen() {
         MessageUtil.printMessage("Other Withdraw");
@@ -12,6 +27,17 @@ public class OtherWithdrawScreen extends Screen {
 
     @Override
     protected void readInput() {
+        String input = doInput();
+        ValidatorUtil validatorUtil = new ValidatorUtil();
+        boolean isWithdrawAmountValid = validatorUtil.isWithdrawAmountValid(input, customer);
+        if (!isWithdrawAmountValid) {
+            this.existScreen = true;
+            return;
+        }
+        this.amount = new BigDecimal(input);
+    }
 
+    public BigDecimal getAmount() {
+        return this.amount;
     }
 }
