0690d06a495f5d52fb123cd6355788f2222084bd
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-22 10:43:33 +0700

    add MESSAGE_DELIMITER, addDelimiter(), deleteLastDelimiter()

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/util/MessageUtil.java
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/util/MessageUtil.java
@@ -2,6 +2,8 @@ package com.mahendracandi.mitrais_atm_simulation.util;
 
 public class MessageUtil {
 
+    public final static String MESSAGE_DELIMITER = ";";
+
     public static void printMessage (String message) {
         System.out.println(message);
     }
@@ -12,5 +14,13 @@ public class MessageUtil {
 
     public static void printInvalidMessage(String message, int value) {
         System.err.printf(message + "\n", value);
+    }
+
+    public String addDelimiter(String message) {
+        return message + MESSAGE_DELIMITER;
+    }
+
+    public String deleteLastDelimiter(String message) {
+        return message.replaceAll(";$", "");
     }
 }
