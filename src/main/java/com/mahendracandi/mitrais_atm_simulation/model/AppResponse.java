2ce2cf2fcb03a5416b23ec5b2644220ceca4f4ab
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-22 10:44:46 +0700

    add AppResponse

---
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/model/AppResponse.java
@@ -0,0 +1,31 @@
+package com.mahendracandi.mitrais_atm_simulation.model;
+
+public class AppResponse<T> {
+    private String message;
+    private boolean status;
+    private T data;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
