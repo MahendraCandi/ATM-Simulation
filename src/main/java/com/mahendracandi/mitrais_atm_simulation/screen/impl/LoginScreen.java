15ec1d896f66223b89084dc01b206857367ff84e
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-22 10:45:22 +0700

    update LoginScreen using LoginController

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/LoginScreen.java
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/LoginScreen.java
@@ -1,42 +1,41 @@
 package com.mahendracandi.mitrais_atm_simulation.screen.impl;
 
+import com.mahendracandi.mitrais_atm_simulation.controller.LoginController;
+import com.mahendracandi.mitrais_atm_simulation.model.AppResponse;
 import com.mahendracandi.mitrais_atm_simulation.model.Customer;
 import com.mahendracandi.mitrais_atm_simulation.screen.Screen;
-import com.mahendracandi.mitrais_atm_simulation.service.CustomerService;
-import com.mahendracandi.mitrais_atm_simulation.service.CustomerServiceImpl;
-import com.mahendracandi.mitrais_atm_simulation.util.ValidatorUtil;
+import com.mahendracandi.mitrais_atm_simulation.util.MessageUtil;
 
 public class LoginScreen extends Screen {
 
     private Customer customer;
-    private final CustomerService customerService = new CustomerServiceImpl();
-    private final ValidatorUtil validatorUtil = new ValidatorUtil();
 
     @Override
     public void showScreen() {
+        boolean exit = false;
+        do {
+            MessageUtil.printMessage("Enter account number: ");
+            String accountNumber = doInput();
+            MessageUtil.printMessage("Enter PIN: ");
+            String pinNumber = doInput();
 
+            LoginController loginController = new LoginController();
+            AppResponse<Customer> appResponse = loginController.doLogin(accountNumber, pinNumber);
+            if (appResponse.getStatus()) {
+                exit = true;
+                customer = appResponse.getData();
+            } else {
+                String errorMessage = appResponse.getMessage();
+                for (String message : errorMessage.split(MessageUtil.MESSAGE_DELIMITER)) {
+                    MessageUtil.printInvalidMessage(message);
+                }
+            }
+        } while (!exit);
     }
 
     @Override
     public void readInput() {
-        AccountNumberScreen accountNumberScreen = new AccountNumberScreen();
-        accountNumberScreen.showScreen();
-        accountNumberScreen.readInput();
-        if (accountNumberScreen.isExistScreen()) return;
-        String accountNumber = accountNumberScreen.getInput();
 
-        PinNumberScreen pinNumberScreen = new PinNumberScreen();
-        pinNumberScreen.showScreen();
-        pinNumberScreen.readInput();
-        if (pinNumberScreen.isExistScreen()) return;
-        String pinNumber = pinNumberScreen.getInput();
-
-        Customer customer = customerService.doLogin(accountNumber, pinNumber);
-
-        boolean isCustomerLoginValid = validatorUtil.isCustomerLoginValid(customer);
-        if (isCustomerLoginValid) {
-            this.customer = customer;
-        }
     }
 
     public Customer getCustomer() {
