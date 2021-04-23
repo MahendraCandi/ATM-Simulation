281d134210167ceed86a6469c6b2a9b9473cf75a
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-23 09:16:54 +0700

    optimize FundTransferConfirmationScreen using controller

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/FundTransferConfirmationScreen.java
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/screen/impl/FundTransferConfirmationScreen.java
@@ -1,31 +1,18 @@
 package com.mahendracandi.mitrais_atm_simulation.screen.impl;
 
-import com.mahendracandi.mitrais_atm_simulation.appEnum.TransactionType;
+import com.mahendracandi.mitrais_atm_simulation.controller.FundTransferController;
+import com.mahendracandi.mitrais_atm_simulation.model.AppResponse;
 import com.mahendracandi.mitrais_atm_simulation.model.Customer;
 import com.mahendracandi.mitrais_atm_simulation.model.FundTransfer;
 import com.mahendracandi.mitrais_atm_simulation.model.Transaction;
 import com.mahendracandi.mitrais_atm_simulation.screen.Screen;
-import com.mahendracandi.mitrais_atm_simulation.service.CustomerService;
-import com.mahendracandi.mitrais_atm_simulation.service.CustomerServiceImpl;
 import com.mahendracandi.mitrais_atm_simulation.util.MessageUtil;
-import com.mahendracandi.mitrais_atm_simulation.util.ValidatorUtil;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public class FundTransferConfirmationScreen extends Screen {
 
-    private FundTransfer fundTransfer;
-    private Customer customer;
-    private final ValidatorUtil validatorUtil = new ValidatorUtil();
-    private final CustomerService customerService = new CustomerServiceImpl();
-
-    public FundTransferConfirmationScreen() {
-    }
-
-    public FundTransferConfirmationScreen(FundTransfer fundTransfer) {
-        this.fundTransfer = fundTransfer;
-    }
+    private final FundTransfer fundTransfer;
+    private final Customer customer;
+    private final FundTransferController fundTransferController = new FundTransferController();
 
     public FundTransferConfirmationScreen(FundTransfer fundTransfer, Customer customer) {
         this.fundTransfer = fundTransfer;
@@ -34,42 +21,21 @@ public class FundTransferConfirmationScreen extends Screen {
 
     @Override
     public void showScreen() {
-        MessageUtil.printMessage(fundTransfer.toConfirmationString());
-        MessageUtil.printMessage("1. Confirm Trx");
-        MessageUtil.printMessage("2. Cancel Trx");
-        MessageUtil.printMessage("Choose option[2]");
-        this.setDefaultInput("2");
-    }
-
-    @Override
-    protected void readInput() {
         boolean exitLoop = false;
         do {
-            showScreen();
-            String option = doInput();
+            MessageUtil.printMessage(fundTransfer.toConfirmationString());
+            MessageUtil.printMessage("1. Confirm Trx");
+            MessageUtil.printMessage("2. Cancel Trx");
+            MessageUtil.printMessage("Choose option[2]");
+            String option = doInput("2");
             switch (option) {
                 case "1":
-                    String destinationAccount = fundTransfer.getDestinationAccount();
-                    boolean isDestinationAccountValid = validatorUtil.isDestinationAccountValid(destinationAccount);
-                    boolean isTransferAmountValid = validatorUtil.isTransferAmountValid(
-                            fundTransfer.getTransferAmount(), customer);
-                    boolean isReferenceNumberValid = validatorUtil.isReferenceNumberValid(
-                            fundTransfer.getReferenceNumber());
-
-                    if (isDestinationAccountValid && isTransferAmountValid && isReferenceNumberValid) {
-                        Customer destinationCustomer = customerService.getCustomerByAccountNumber(destinationAccount);
-                        TransactionType transactionType = TransactionType.FUND_TRANSFER;
-
-                        Transaction transaction = new Transaction();
-                        transaction.setTransactionType(transactionType);
-                        transaction.setCustomer(customer);
-                        transaction.setAmount(new BigDecimal(fundTransfer.getTransferAmount()));
-                        transaction.setDate(LocalDateTime.now());
-                        transaction.setDestinationAccount(destinationCustomer);
-                        transaction.setReferenceNumber(fundTransfer.getReferenceNumber());
-
-                        SummaryScreen summaryScreen = new SummaryScreen(transaction, transactionType);
-                        summaryScreen.readInput();
+                    AppResponse<Transaction> appResponse = fundTransferController.readFundTransferAmount(fundTransfer, customer);
+                    if (appResponse.getStatus()) {
+                        SummaryScreen summaryScreen = new SummaryScreen(appResponse.getData());
+                        summaryScreen.showScreen();
+                    } else {
+                        MessageUtil.printAllErrorMessage(appResponse.getMessage());
                     }
                     exitLoop = true;
                     break;
@@ -77,7 +43,11 @@ public class FundTransferConfirmationScreen extends Screen {
                     exitLoop = true;
                     break;
             }
-        }while (!exitLoop);
+        } while (!exitLoop);
+    }
+
+    @Override
+    protected void readInput() {
 
     }
 }
