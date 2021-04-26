ffe5ea3723836565144e1f4356744a87311fad2d
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-26 15:04:21 +0700

    update separate logic withdraw controller

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/controller/WithdrawController.java
+++ src/main/java/com/mahendracandi/mitrais_atm_simulation/controller/WithdrawController.java
@@ -4,46 +4,39 @@ import com.mahendracandi.mitrais_atm_simulation.appenum.TransactionType;
 import com.mahendracandi.mitrais_atm_simulation.model.AppResponse;
 import com.mahendracandi.mitrais_atm_simulation.model.Customer;
 import com.mahendracandi.mitrais_atm_simulation.model.Transaction;
-import com.mahendracandi.mitrais_atm_simulation.util.AppUtil;
+import com.mahendracandi.mitrais_atm_simulation.model.ValidationMessage;
 import com.mahendracandi.mitrais_atm_simulation.util.MessageUtil;
+import com.mahendracandi.mitrais_atm_simulation.validation.transaction.AmountValidator;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import static com.mahendracandi.mitrais_atm_simulation.util.ValidatorUtil.ValidationResult.*;
 import static com.mahendracandi.mitrais_atm_simulation.util.ValidatorUtil.ValidationResult.INVALID_AMOUNT;
+import static com.mahendracandi.mitrais_atm_simulation.util.ValidatorUtil.ValidationResult.SUCCESS;
 
 public class WithdrawController {
 
-    private final AppUtil appUtil = new AppUtil();
     private final MessageUtil messageUtil = new MessageUtil();
 
     public AppResponse<Transaction> readWithdrawAmount(Customer customer, String amountStr) {
         AppResponse<Transaction> appResponse = new AppResponse<>();
         Transaction transaction = null;
         BigDecimal amount = null;
-        boolean isResultValid = true;
-        String message = "";
+        boolean isResultValid = false;
+        String message;
 
         try {
             amount = new BigDecimal(amountStr);
-            if (!appUtil.isValueMultipleOfTen(amount.intValue())) throw new NumberFormatException();
 
-            if (appUtil.isValueMoreThanMaximumAmount(amount.intValue())) {
-                isResultValid = false;
-                message += messageUtil.addDelimiter(INVALID_MAXIMUM_AMOUNT.value);
-            } else if (appUtil.isValueLessThanMinimumAmount(amount.intValue())) {
-                isResultValid = false;
-                message += messageUtil.addDelimiter(INVALID_MINIMUM_AMOUNT.value);
+            ValidationMessage checkBalance = new AmountValidator().validateBalance(customer, amount);
+            if (checkBalance.isNotSuccess()) {
+                message = messageUtil.joinMessages(checkBalance.getErrorMessages());
             } else {
-                if (customer.getBalance().compareTo(amount) < 0) {
-                    isResultValid = false;
-                    message += messageUtil.addDelimiter(INSUFFICIENT_BALANCE.value + " $" + amount);
-                }
+                isResultValid = true;
+                message = SUCCESS.value;
             }
         } catch (NumberFormatException e) {
-            isResultValid = false;
-            message += messageUtil.addDelimiter(INVALID_AMOUNT.value);
+            message = messageUtil.addDelimiter(INVALID_AMOUNT.value);
         }
 
         if (isResultValid) {
@@ -52,12 +45,10 @@ public class WithdrawController {
             transaction.setCustomer(customer);
             transaction.setAmount(amount);
             transaction.setDate(LocalDateTime.now());
-
-            message = SUCCESS.value;
         }
 
         appResponse.setData(transaction);
-        appResponse.setMessage(messageUtil.deleteLastDelimiter(message));
+        appResponse.setMessage(message);
         appResponse.setStatus(isResultValid);
 
         return appResponse;
