8291c91fe4e319862c760cd6c8cbd23c0b288ae9
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-27 17:23:28 +0700

    delete unnecessary file

--- src/main/java/com/mahendracandi/mitrais_atm_simulation/validation/ReferenceNumberValidator.java
+++
@@ -1,7 +0,0 @@
-package com.mahendracandi.mitrais_atm_simulation.validation;
-
-import com.mahendracandi.mitrais_atm_simulation.model.ValidationMessage;
-
-public interface ReferenceNumberValidator{
-    ValidationMessage validateNumber(String value);
-}
